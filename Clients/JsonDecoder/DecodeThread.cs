9d80ace8e5cdfa74d4cba17107ba64385e004584
Ryuuji Yoshimoto <ryuuji@calil.jp>
2018-08-15 09:25:26 +0900

    add json utility

---
+++ Clients/JsonDecoder/DecodeThread.cs
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2011 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+using ZXing;
+using ZXing.Client.Result;
+
+namespace CommandLineDecoder
+{
+    /// <summary>
+    /// One of a pool of threads which pulls images off the Inputs queue and decodes them in parallel.
+    /// @see CommandLineRunner
+    /// </summary>
+    internal sealed class DecodeThread
+    {
+        private int successful;
+        private readonly Config config;
+        private readonly Inputs inputs;
+        public string ResultString { get; private set; }
+
+        public DecodeThread(Config config, Inputs inputs)
+        {
+            this.config = config;
+            this.inputs = inputs;
+        }
+
+        public void run()
+        {
+            ResultString = String.Empty;
+            while (true)
+            {
+                String input = inputs.getNextInput();
+                if (input == null)
+                {
+                    break;
+                }
+                try
+                {
+                    Result[] results = decodeMulti(new Uri(Path.GetFullPath(input)), input, config.Hints);
+                    if (results != null)
+                    {
+                        successful++;
+                    }
+                }
+                catch (IOException exc)
+                {
+                    Console.WriteLine(exc.ToString());
+                }
+            }
+        }
+
+        public int getSuccessful()
+        {
+            return successful;
+        }
+
+        private static void dumpResultMulti(String input, Result[] results)
+        {
+            int pos = input.LastIndexOf('.');
+            if (pos > 0)
+            {
+                input = input.Substring(0, pos);
+            }
+            using (var stream = File.CreateText(input + ".txt"))
+            {
+                foreach (var result in results)
+                {
+                    stream.WriteLine(result.Text);
+                }
+            }
+        }
+
+        private Result[] decodeMulti(Uri uri, string originalInput, IDictionary<DecodeHintType, object> hints)
+        {
+            Bitmap image;
+            try
+            {
+                image = (Bitmap)Bitmap.FromFile(uri.LocalPath);
+            }
+            catch (Exception)
+            {
+                throw new FileNotFoundException("Resource not found: " + uri);
+            }
+
+            using (image)
+            {
+                LuminanceSource source;
+                if (config.Crop == null)
+                {
+                    source = new BitmapLuminanceSource(image);
+                }
+                else
+                {
+                    int[] crop = config.Crop;
+                    source = new BitmapLuminanceSource(image).crop(crop[0], crop[1], crop[2], crop[3]);
+                }
+
+                var reader = new BarcodeReader { AutoRotate = config.AutoRotate };
+                foreach (var entry in hints)
+                    reader.Options.Hints.Add(entry.Key, entry.Value);
+                Result[] results = reader.DecodeMultiple(source);
+                if (results != null && results.Length > 0)
+                {
+                        foreach (var result in results)
+                        {
+                            ParsedResult parsedResult = ResultParser.parseResult(result);
+                            var resultString = originalInput + " (format: " + result.BarcodeFormat + ", type: " + parsedResult.Type + "):" + Environment.NewLine;
+                            for (int i = 0; i < result.ResultPoints.Length; i++)
+                            {
+                                ResultPoint rp = result.ResultPoints[i];
+                                Console.Out.WriteLine("  Point " + i + ": (" + rp.X + ',' + rp.Y + ')');
+                            }
+                            resultString += "Raw result:" + Environment.NewLine + result.Text + Environment.NewLine;
+                            resultString += "Parsed result:" + Environment.NewLine + parsedResult.DisplayResult + Environment.NewLine;
+
+                            Console.Out.WriteLine(resultString);
+                            ResultString = resultString;
+                        }
+                    return results;
+                }
+                else
+                {
+                    var resultString = originalInput + ": No barcode found";
+                    Console.Out.WriteLine(resultString);
+                    ResultString = resultString;
+                }
+                return null;
+            }
+        }
+
+    }
+}
---
+++ Clients/JsonDecoder/Program.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Copyright 2012 ZXing.Net authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using ZXing;
+
+namespace CommandLineDecoder
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                printUsage();
+                return;
+            }
+
+            Config config = new Config();
+            Inputs inputs = new Inputs();
+            config.AutoRotate = true;
+            foreach (var arg in args)
+            {
+                if (arg.StartsWith("--crop"))
+                {
+                    int[] crop = new int[4];
+                    String[] tokens = arg.Substring(7).Split(',');
+                    for (int i = 0; i < crop.Length; i++)
+                    {
+                        crop[i] = int.Parse(tokens[i]);
+                    }
+                    config.Crop = crop;
+                }
+                else if (arg.StartsWith("--threads") && arg.Length >= 10)
+                {
+                    int threadsCount = int.Parse(arg.Substring(10));
+                    if (threadsCount > 1)
+                    {
+                        config.Threads = threadsCount;
+                    }
+                }
+                else if (arg.StartsWith("-"))
+                {
+                    Console.Error.WriteLine("Unknown command line option " + arg);
+                    printUsage();
+                    return;
+                }
+            }
+
+            config.Hints = buildHints(config);
+
+            foreach (var arg in args)
+            {
+                if (!arg.StartsWith("--"))
+                {
+                    addArgumentToInputs(arg, config, inputs);
+                }
+            }
+
+            var threads = new Dictionary<Thread, DecodeThread>(Math.Min(config.Threads, inputs.getInputCount()));
+            var decodeObjects = new List<DecodeThread>();
+            for (int x = 0; x < config.Threads; x++)
+            {
+                var decodeThread = new DecodeThread(config, inputs);
+                decodeObjects.Add(decodeThread);
+                var thread = new Thread(decodeThread.run);
+                threads.Add(thread, decodeThread);
+                thread.Start();
+            }
+
+            int successful = 0;
+            foreach (var thread in threads.Keys)
+            {
+                thread.Join();
+                successful += threads[thread].getSuccessful();
+            }
+
+            int total = inputs.getInputCount();
+            if (total > 1)
+            {
+                Console.Out.WriteLine("\nDecoded " + successful + " files out of " + total +
+                    " successfully (" + (successful * 100 / total) + "%)\n");
+            }
+        }
+
+        // Build all the inputs up front into a single flat list, so the threads can atomically pull
+        // paths/URLs off the queue.
+        private static void addArgumentToInputs(String argument, Config config, Inputs inputs)
+        {
+            inputs.addInput(argument);
+        }
+
+        // Manually turn on all formats, even those not yet considered production quality.
+        private static IDictionary<DecodeHintType, object> buildHints(Config config)
+        {
+            var hints = new Dictionary<DecodeHintType, Object>();
+            var vector = new List<BarcodeFormat>(8)
+                    {
+                       BarcodeFormat.UPC_A,
+                       BarcodeFormat.UPC_E,
+                       BarcodeFormat.EAN_13,
+                       BarcodeFormat.EAN_8,
+                       BarcodeFormat.RSS_14,
+                       BarcodeFormat.CODABAR,
+                       BarcodeFormat.RSS_EXPANDED,
+                       BarcodeFormat.CODE_39
+                    };
+            hints[DecodeHintType.POSSIBLE_FORMATS] = vector;
+            hints[DecodeHintType.TRY_HARDER] = true;
+            return hints;
+        }
+
+        private static void printUsage()
+        {
+            Console.Out.WriteLine("Decode barcode images using the ZXing library\n");
+            Console.Out.WriteLine("usage: CommandLineRunner { file | dir | url } [ options ]");
+            Console.Out.WriteLine("  --crop=left,top,width,height: Only examine cropped region of input image(s)");
+            Console.Out.WriteLine("  --threads=n: The number of threads to use while decoding");
+        }
+    }
+}
